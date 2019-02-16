5fbe8335b82118569995427a2c12a47956f240f2
EUGINELETHAL <ochungeugine@gmail.com>
2019-02-16 05:48:50 +0300

    added serializers file and requirements file

---
+++ myblog/blog_api/serializers.py
@@ -0,0 +1,37 @@
+rom django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+from .models import Article, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(
+        max_length=None,
+        min_length=None,
+        allow_blank=False)
+
+    password = serializers.CharField(
+        max_length=100,
+        style={'input_type': 'password'},
+        required=True,
+        write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
--- myblog/blog_api/views.py
+++ myblog/blog_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Category, Article
+from .serializers import CategorySerializer, ArticleSerializer, UserSerializer
+
+
+#........
+class Areticle(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
